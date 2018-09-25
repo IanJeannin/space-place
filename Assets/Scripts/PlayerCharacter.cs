c3010dbc754e526689a42f98098b8c29e18dfa0b
IanJeannin <36770934+IanJeannin@users.noreply.github.com>
2018-09-25 14:28:54 -0500

    Delete Git For Unity

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class PlayerCharacter : MonoBehaviour {
 
-    public Rigidbody2D myRigidBody;
+    [SerializeField]
+    Rigidbody2D myRigidBody;
     [SerializeField]
     private float speed=10;
 	// Use this for initialization
