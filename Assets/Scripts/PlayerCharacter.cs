d05b65a7beb974241cb9c6e6ea42a7f5fcc01d33
IanJeannin <36770934+IanJeannin@users.noreply.github.com>
2018-09-25 14:07:57 -0500

    Update Script Readability

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerCharacter : MonoBehaviour {
 
     public Rigidbody2D myRigidBody;
+    [SerializeField]
+    private float speed=10;
 	// Use this for initialization
 	void Start ()
     {
@@ -18,21 +20,21 @@ public class PlayerCharacter : MonoBehaviour {
         if(Input.GetKey(KeyCode.D))
         {
             //TODO: Move Character Right
-            myRigidBody.velocity = new Vector2(5,0);
+            myRigidBody.velocity = new Vector2(speed,0);
             
         }
         
         if (Input.GetKey(KeyCode.S))
         {
-            myRigidBody.velocity = new Vector2(0, -5);
+            myRigidBody.velocity = new Vector2(0, -speed);
         }
         else if (Input.GetKey(KeyCode.A))
         {
-            myRigidBody.velocity = new Vector2(-5, 0);
+            myRigidBody.velocity = new Vector2(-speed, 0);
         }
         else if (Input.GetKey(KeyCode.W))
         {
-            myRigidBody.velocity = new Vector2(0, 5);
+            myRigidBody.velocity = new Vector2(0, speed);
         }
         //This is the syntax for printing to the console
         //Debug.Log("Hello World");
