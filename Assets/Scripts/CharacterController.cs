8c1e8c1d9ce47c9c845b0e0c9ecab6d0c283f236
IanJeannin <36770934+IanJeannin@users.noreply.github.com>
2018-10-23 15:04:31 -0500

    Animations And Character Controller

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour {
+
+    [SerializeField]
+    private float maxSpeed = 10f;
+    private bool isFacingRight = true;
+    private bool isOnGround;
+
+    Animator anim;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        float move = Input.GetAxis("Horizontal");
+        anim.SetFloat("Speed", Mathf.Abs(move));
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(move * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
+
+        if(move>0&&!isFacingRight)
+        {
+            Flip();
+        }
+        if(move<0&&isFacingRight)
+        {
+            Flip();
+        }
+
+    }
+
+    void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
