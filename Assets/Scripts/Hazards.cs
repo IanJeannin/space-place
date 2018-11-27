05d39984c7552e1900a13d505eb537be0943e80a
IanJeannin <36770934+IanJeannin@users.noreply.github.com>
2018-11-27 10:49:30 -0600

    Add button functionality
    TODO: Change button image to inactive, allow button to be reactivated, add prefab

--- Assets/Scripts/Hazards.cs
+++ Assets/Scripts/Hazards.cs
@@ -8,37 +8,55 @@ public class Hazards : MonoBehaviour {
     private bool isOn = true; //Used to determine whether or not the hazard is currently showing
     private float timeToChange = 0.01f; //Used to determine the time between hazard changing show state
     private float timeToAdd = 0.01f; //Add to timeToChange to continually change the hazards renderer
+    [SerializeField]
+    Buttons button; //Button associated with this gate
+
     private void Update()
     {
-        if (transform.parent.name == "Hazards")
+        if (button.GetActive() == true) //So long as the button associated with this gate has not been pushed
         {
-            if (Time.time > timeToChange) //If the time since startup is a multiple of the time to change, change isOn
+            if (transform.parent.name == "Hazards") //Used to only access the lazer image of the gate
             {
-                isOn = !isOn; //isOn equals whatever it's not
-                timeToChange += timeToAdd;
-            }
-
-            if (isOn == true) //If isOn is true
-            {
-                SpriteRenderer[] hazard = GetComponentsInChildren<SpriteRenderer>(); //Get all rendrers in child objects
-                foreach (Renderer x in hazard) //For every renderer in the child components
+                if (Time.time > timeToChange) //If the time since startup is a multiple of the time to change, change isOn
                 {
-                    if (x.gameObject.transform.parent.name == "LazerGate" || transform.name == "FallDeath") //Checks that it's only getting the lazer parts of hazard
-                    {
-                        x.enabled = true; //Let the hazard show
-                    }
+                    isOn = !isOn; //isOn equals whatever it's not
+                    timeToChange += timeToAdd;
+                }
 
+                if (isOn == true) //If isOn is true
+                {
+                    SpriteRenderer[] hazard = GetComponentsInChildren<SpriteRenderer>(); //Get all rendrers in child objects
+                    foreach (Renderer x in hazard) //For every renderer in the child components
+                    {
+                        if (x.gameObject.transform.parent.name == "LazerGate" || transform.name == "FallDeath") //Checks that it's only getting the lazer parts of hazard
+                        {
+                            x.enabled = true; //Let the hazard show
+                        }
+
+                    }
+                }
+                else //Otherwise
+                {
+                    SpriteRenderer[] hazard = GetComponentsInChildren<SpriteRenderer>();
+                    foreach (Renderer x in hazard)
+                    {
+                        if (x.gameObject.transform.parent.name == "LazerGate" || transform.name == "FallDeath") //Checks that it's only getting the lazer parts of hazard
+                        {
+                            x.enabled = false; //Don't
+                        }
+                    }
                 }
             }
-            else //Otherwise
+        }
+        else
+        {
+            this.GetComponent<BoxCollider2D>().enabled = false;
+            SpriteRenderer[] hazard = GetComponentsInChildren<SpriteRenderer>();
+            foreach (Renderer x in hazard)
             {
-                SpriteRenderer[] hazard = GetComponentsInChildren<SpriteRenderer>();
-                foreach (Renderer x in hazard)
+                if (x.gameObject.transform.parent.name == "LazerGate") //Checks that it's only getting the lazer parts of hazard
                 {
-                    if (x.gameObject.transform.parent.name == "LazerGate" || transform.name == "FallDeath") //Checks that it's only getting the lazer parts of hazard
-                    {
-                        x.enabled = false; //Don't
-                    }
+                    x.enabled = false; //Don't
                 }
             }
         }
